9598f4682c698ff4babea7718227656efc2f177a
Hreiðar Ingi <hreidar220@gmail.com>
2023-09-18 21:47:43 +0000

    projects section

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
+import vedurstodinImg from "@/public/vedurstodinImg.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
@@ -27,5 +30,15 @@ export const links = [
   {
     name: "Contact",
     hash: "#contact",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Veðurstöðin",
+    description:
+      "I built this website myself with NextJS, TypeScript TailwindCSS, OpenAI and more. ChatGPT gives a summary of weather data for a chosen city along with graphs",
+    tags: ["React", "Next.js", "Vercel", "Tailwind", "OpenAI"],
+    imageUrl: vedurstodinImg,
   },
 ] as const;
