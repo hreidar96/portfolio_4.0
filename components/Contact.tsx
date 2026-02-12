7679b3f9b09ab1fa3e2e981270d23681bde8f681
Hreiðar Ingi <hreidar220@gmail.com>
2026-02-12 23:23:40 +0000

    contrast fix and update image

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -60,11 +60,11 @@ export default function Contact() {
       </p>
 
       <form
-        className="mt-10 flex flex-col dark:text-black"
+        className="mt-10 flex flex-col"
         action={handleSubmit}
       >
         <input
-          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white/10 dark:focus:bg-white/20 transition-all dark:outline-none dark:text-white"
           name="senderEmail"
           type="email"
           required
@@ -72,7 +72,7 @@ export default function Contact() {
           placeholder="Your email"
         />
         <textarea
-          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white/10 dark:focus:bg-white/20 transition-all dark:outline-none dark:text-white"
           name="message"
           placeholder="Your message"
           required
